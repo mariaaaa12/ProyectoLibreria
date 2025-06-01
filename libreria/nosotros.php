9bd5634db5ca2443fe2d12d6716bbf3bc99c9de2
Angeles <maria.fi1701@gmail.com>
2025-05-31 21:34:44 -0500

    se carga la base de datos

---
+++ libreria/nosotros.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>About - Calvin</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/vendor.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script defer src="js/fontawesome/all.min.js"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+</head>
+
+<body id="top">
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader"> 
+    	<div id="loader"></div>
+    </div>
+
+
+    <!-- header
+    ================================================== -->
+    <header class="s-header s-header--opaque">
+
+        <div class="s-header__logo">
+            <a class="logo" href="index.php">
+                <img src="images/logo.png" alt="Homepage">
+            </a>
+        </div>
+
+               
+    </header> <!-- end s-header -->
+
+
+    <!-- content
+    ================================================== -->
+    <section class="s-content">
+
+        <div class="row">
+            <div class="column large-12">
+
+                <article class="s-content__entry">
+
+                    <div class="s-content__media">
+                        <img src="images/logo.png" alt="Homepage">
+                                
+
+                    </div> <!-- end s-content__media -->
+
+                    <div class="s-content__entry-header">
+                        <h1 class="s-content__title">Tinta Paginas  y Encuentros</h1>
+                    </div> <!-- end s-content__entry-header -->
+
+                    <div class="s-content__primary">
+
+                        <div class="s-content__page-content">
+
+                            <p class="lead">
+                            Un sistema inteligente que personaliza tus lecturas y transforma por completo la experiencia de descubrir libros, adaptándose a tus verdaderos gustos.
+                            </p>
+                            
+                            <p>
+                            Imagina una plataforma que no solo te recomienda libros al azar, sino que realmente te conoce. Un sistema inteligente que aprende de tus hábitos de lectura,
+                            tus emociones frente a ciertos géneros y tus autores favoritos, para proponerte lecturas que resuenan contigo en un nivel profundo. 
+                            Esta tecnología va más allá de los algoritmos tradicionales: analiza tus preferencias de forma dinámica, 
+                            se adapta a tus cambios de humor o intereses, y te guía hacia historias que probablemente amarás, incluso antes de que tú mismo lo sepas.
+                            </p> 
+
+                            <p>
+                            Ya no tendrás que perder tiempo buscando qué leer o sintiéndote frustrado con recomendaciones genéricas. 
+                            Este sistema transforma la manera en la que descubres nuevos libros, convirtiendo cada sugerencia en una experiencia personalizada, íntima y profundamente 
+                            conectada con quién eres como lector. 
+                            Es como tener un asesor literario personal, siempre atento a lo que realmente te emociona, inspira o desafía.
+
+                            </p>
+
+                            <p>
+                                Bienvenido al futuro de la lectura: inteligente, intuitivo y hecho a tu medida.
+                            </p>
+
+                            
+
+                            <br>
+
+                            <div class="row block-large-1-2 block-tab-full s-content__blocks">
+                                <div class="column">
+                                    <h4>La Tecnología que lo Hace Posible</h4>
+                                    <p>
+                                    Usa análisis semántico para entender el estilo, los temas y lacomplejidad de los libros, como si pudiera “leer entre líneas”. 
+                                    Tambiénaplica análisis de sentimientos para captar las emociones queprovocan las historias y conectar con el estado de ánimo del lector.Gracias a 
+                                    redes neuronales profundas, el sistema aprende de cadausuario: sus gustos, lecturas previas y reacciones, afinando lasrecomendaciones con cada interacción.
+                                    </p>
+                                </div>
+
+                                <div class="column">
+                                    <h4>Cuando los libros te entienden.</h4>
+                                    <p>
+                                    Leer deja de ser una simple actividad para convertirse en un refugio emocional, un espacio donde cada historia 
+                                    parece escrita especialmente para ti. Este sistema inteligente no solo entiende qué libros te gustan, 
+                                    sino que intuye lo que necesitas sentir en cada momento: consuelo en los días grises, inspiración cuando buscas motivación,
+                                     o una chispa de alegría cuando lo cotidiano pesa. Es como si cada recomendación te abrazara, conectando con lo más 
+                                    profundo de tus emociones y devolviéndote a ese lugar íntimo donde los libros hablan directamente al corazón.
+                                    </p>
+                                </div>
+
+
+                            </div>
+                        </div> <!-- end s-entry__page-content -->
+
+                    </div> <!-- end s-content__primary -->
+                </article> <!-- end entry -->
+
+            </div> <!-- end column -->
+        </div> <!-- end row -->
+
+    </section> <!-- end s-content -->
+
+
+ <footer class="s-footer">
+
+        <div class="s-footer__main">
+
+            <div class="row">
+
+                <div class="column large-3 medium-6 tab-12 s-footer__info">
+
+                    <h5>Acerca de nosotros</h5>
+
+                    <p>
+                        "Tu próximo libro favorito te está esperando… <br>
+                        Nosotros sabemos cuál es, porque entendemos tus pasiones, tus emociones y aquello que realmente
+                        disfrutas leer. <br>
+                        Déjanos guiarte a historias que parecen escritas para ti."
+                    </p>
+
+                </div> <!-- end s-footer__info -->
+
+                <div class="column large-2 medium-3 tab-6 s-footer__site-links">
+
+                    <h5>Generos disponibles</h5>
+
+                    <ul>
+                         <li><a href="#0">Drama</a></li>
+                        <li><a href="#0">Policial</a></li>
+                        <li><a href="#0">Romance</a></li>
+                        <li><a href="#0">Ciencia Ficcion</a></li>
+                        <li><a href="#0">Terror</a></li>
+                        <li><a href="#0">Tragedia</a></li>
+                        <li><a href="#0">Comedia</a></li>
+                        <li><a href="#0">Melodrama</a></li>
+                        <li><a href="#0">Fantasia</a></li>
+                        <li><a href="#0">Distopia</a></li>
+                        <li><a href="#0">Privacy Policy</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__site-links -->  
+
+                <div class="column large-2 medium-3 tab-6 s-footer__social-links">
+
+                    <h5>Siguenos</h5>
+
+                    <ul>
+                        <li><a href="#0">Twitter</a></li>
+                        <li><a href="#0">Facebook</a></li>
+                        <li><a href="#0">Pinterest</a></li>
+                        <li><a href="#0">Instagram</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__social links --> 
+
+                <div class="column large-3 medium-6 tab-12 s-footer__subscribe">
+
+                    <h5>Comparte tu experiencia y ¡Hasta la próxima aventura literaria!</h5>
+
+                    <p>Aquí siempre habrá un libro esperándote para abrir sus páginas y 
+                        llevarte a otro mundo.
+                        ¡Vuelve pronto a tu rincón favorito de historias! 📖❤️.</p>
+
+                    <div class="subscribe-form">
+                
+                        <form id="mc-form" class="group" onsubmit="window.location.href='index.php'; return false;" novalidate="true">
+                        <input type="submit" name="salir" value="Salir">
+                        </form>
+
+                    </div>
+
+                </div> <!-- end s-footer__subscribe -->
+
+            </div> <!-- end row -->
+
+        </div> <!-- end s-footer__main -->
+
+        <div class="s-footer__bottom">
+            <div class="row">
+                <div class="column">
+                    <div class="ss-copyright">
+                        <span>© Udenar Ext Ipiales</span> 
+                        <span>Design by Tinta Paginas y Encuentros</span>
+                    </div> <!-- end ss-copyright -->
+                </div>
+            </div> 
+
+            <div class="ss-go-top">
+                <a class="smoothscroll" title="Back to Top" href="#top">
+                    <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5l.354-.354L7.5.793l-.354.353.354.354zm-.354.354l4 4 .708-.708-4-4-.708.708zm0-.708l-4 4 .708.708 4-4-.708-.708zM7 1.5V14h1V1.5H7z" fill="currentColor"></path></svg>
+                </a>
+            </div> <!-- subir  -->
+        </div> <!-- end s-footer__bottom -->
+
+   </footer> <!-- end s-footer -->
+
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.5.0.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ libreria/soporte.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Ayuda/Soporte</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/vendor.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script defer src="js/fontawesome/all.min.js"></script>
+
+
+</head>
+
+<body id="top">
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader"> 
+    	<div id="loader"></div>
+    </div>
+
+
+    <!-- header
+    ================================================== -->
+    <header class="s-header s-header--opaque">
+
+        <div class="s-header__logo">
+            <a class="logo" href="index.php">
+                <img src="images/logo.png" alt="logo">
+            </a>
+        </div>
+
+    </header> <!-- end s-header -->
+
+
+    <!-- content
+    ================================================== -->
+    <section class="s-content">
+
+        <div class="row">
+            <div class="column large-12">
+
+                <article class="s-content__entry">
+
+                    <div class="s-content__media">
+                        <img src="images/thumbs/contact/contact-1050.jpg" 
+                                srcset="images/thumbs/contact/contact-2100.jpg 2100w, 
+                                        images/thumbs/contact/contact-1050.jpg 1050w, 
+                                        images/thumbs/contact/contact-525.jpg 525w" sizes="(max-width: 2100px) 100vw, 2100px" alt="">
+                    </div> <!-- end s-content__media -->
+
+                    <div class="s-content__entry-header">
+                        <h1 class="s-content__title">🧭 Guía del Lector Perdido <br><br>
+                        ¿Todo se ve confuso? 
+                        Aquí encontrarás respuestas para seguir leyendo sin tropiezos.</h1>
+                    </div> <!-- end s-content__entry-header -->
+
+                    <div class="s-content__primary">
+
+                        <div class="s-content__page-content">
+
+                            <p class="lead">
+                            ¿Tienes problemas para iniciar sesión, registrarte o simplemente explorar 
+                            los libros disponibles?
+                            </p> 
+
+                            <p>
+                             ¡Tranquilo! Estamos aquí para ayudarte. En esta sección encontrarás respuestas claras a 
+                             las preguntas más comunes sobre el acceso a nuestra plataforma, 
+                             cómo encontrar tus lecturas favoritas, y cómo personalizar tu experiencia como lector.
+                            </p>
+                            <p>
+                             Si necesitas una mano extra, también puedes escribirnos
+                            y te orientaremos paso a paso para que sigas disfrutando de cada página sin interrupciones.
+                            </p>
+
+                            <br>
+                            <br>
+
+                            <div class="row block-large-1-2 block-tab-full s-content__blocks">
+                                <div class="column">
+                                    <h4>Donde puedes encontrarnos</h4>
+                                    <p>
+                                    Dirección: Calle 1 No. 3-81, Barrio La Floresta<br>
+                                    piales, Nariño, Colombia.<br>
+                                    📞 (602) 724 4309, Extensión 3000.
+                                    </p>
+                                </div>
+
+                                <div class="column">
+                                    <h4>Contact Info</h4>
+                                    <p>
+                                    soporte@tintapaginasyencuentros.com<br>
+                                    ing@tintapaginasyencuentros.com <br>
+                                    📞 (602) 724 4309, Extensión 3000.
+                                    </p> 
+                                </div>
+                            </div> <!-- end s-content__blocks -->
+
+                            <form name="cForm" id="cForm" class="s-content__form" method="post" action="">
+                                <fieldset>
+
+                                    <div class="form-field">
+                                        <input name="cName" type="text" id="cName" class="h-full-width h-remove-bottom" placeholder="Tu Nombre" value="">
+                                    </div>
+
+                                    <div class="form-field">
+                                        <input name="cEmail" type="text" id="cEmail" class="h-full-width h-remove-bottom" placeholder="Tu E-mail" value="">
+                                    </div>
+
+                                    <div class="message form-field">
+                                        <textarea name="cMessage" id="cMessage" class="h-full-width" placeholder="Tu inquietud o Solicitud" ></textarea>
+                                    </div>
+
+                                    <br>
+                                    <button type="submit" class="submit btn btn--primary h-full-width">Enviar </button>
+
+                                </fieldset>
+                            </form> <!-- end form -->
+
+                        </div> <!-- end s-entry__page-content -->
+
+                    </div> <!-- end s-content__primary -->
+                </article> <!-- end entry -->
+
+            </div> <!-- end column -->
+        </div> <!-- end row -->
+
+    </section> <!-- end s-content -->
+
+
+    <footer class="s-footer">
+
+        <div class="s-footer__main">
+
+            <div class="row">
+
+                <div class="column large-3 medium-6 tab-12 s-footer__info">
+
+                    <h5>Acerca de nosotros</h5>
+
+                    <p>
+                        "Tu próximo libro favorito te está esperando… <br>
+                        Nosotros sabemos cuál es, porque entendemos tus pasiones, tus emociones y aquello que realmente
+                        disfrutas leer. <br>
+                        Déjanos guiarte a historias que parecen escritas para ti."
+                    </p>
+
+                </div> <!-- end s-footer__info -->
+
+                <div class="column large-2 medium-3 tab-6 s-footer__site-links">
+
+                    <h5>Generos disponibles</h5>
+
+                    <ul>
+                         <li><a href="#0">Drama</a></li>
+                        <li><a href="#0">Policial</a></li>
+                        <li><a href="#0">Romance</a></li>
+                        <li><a href="#0">Ciencia Ficcion</a></li>
+                        <li><a href="#0">Terror</a></li>
+                        <li><a href="#0">Tragedia</a></li>
+                        <li><a href="#0">Comedia</a></li>
+                        <li><a href="#0">Melodrama</a></li>
+                        <li><a href="#0">Fantasia</a></li>
+                        <li><a href="#0">Distopia</a></li>
+                        <li><a href="#0">Privacy Policy</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__site-links -->  
+
+                <div class="column large-2 medium-3 tab-6 s-footer__social-links">
+
+                    <h5>Siguenos</h5>
+
+                    <ul>
+                        <li><a href="#0">Twitter</a></li>
+                        <li><a href="#0">Facebook</a></li>
+                        <li><a href="#0">Pinterest</a></li>
+                        <li><a href="#0">Instagram</a></li>
+                    </ul>
+
+                </div> <!-- end s-footer__social links --> 
+
+                <div class="column large-3 medium-6 tab-12 s-footer__subscribe">
+
+                    <h5>Comparte tu experiencia y ¡Hasta la próxima aventura literaria!</h5>
+
+                    <p>Aquí siempre habrá un libro esperándote para abrir sus páginas y 
+                        llevarte a otro mundo.
+                        ¡Vuelve pronto a tu rincón favorito de historias! 📖❤️.</p>
+
+                    <div class="subscribe-form">
+                
+                        <form id="mc-form" class="group" onsubmit="window.location.href='index.php'; return false;" novalidate="true">
+                        <input type="submit" name="salir" value="Salir">
+                        </form>
+
+                    </div>
+
+                </div> <!-- end s-footer__subscribe -->
+
+            </div> <!-- end row -->
+
+        </div> <!-- end s-footer__main -->
+
+        <div class="s-footer__bottom">
+            <div class="row">
+                <div class="column">
+                    <div class="ss-copyright">
+                        <span>© Udenar Ext Ipiales</span> 
+                        <span>Design by Tinta Paginas y Encuentros</span>
+                    </div> <!-- end ss-copyright -->
+                </div>
+            </div> 
+
+            <div class="ss-go-top">
+                <a class="smoothscroll" title="Back to Top" href="#top">
+                    <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5l.354-.354L7.5.793l-.354.353.354.354zm-.354.354l4 4 .708-.708-4-4-.708.708zm0-.708l-4 4 .708.708 4-4-.708-.708zM7 1.5V14h1V1.5H7z" fill="currentColor"></path></svg>
+                </a>
+            </div> <!-- subir  -->
+        </div> <!-- end s-footer__bottom -->
+
+   </footer> <!-- end s-footer -->
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.5.0.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
